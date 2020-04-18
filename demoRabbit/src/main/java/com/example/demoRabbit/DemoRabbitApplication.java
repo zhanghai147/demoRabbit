cc5261a98676ccfe300b41f840a21f5715d674ef
zhanghai147 <63179012+zhanghai147@users.noreply.github.com>
2020-04-18 23:19:06 +0800

    Rabbit

---
+++ demoRabbit/src/main/java/com/example/demoRabbit/DemoRabbitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoRabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRabbitApplication.class, args);
+	}
+
+}
---
+++ demoRabbit/src/test/java/com/example/demoRabbit/DemoRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoRabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
